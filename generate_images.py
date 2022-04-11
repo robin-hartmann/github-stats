447ce10f64c62f73d7b4a9cb61d992fa8b8053ef
Robin Hartmann <contact.robin.hartmann@gmail.com>
2022-04-11 19:16:13 +0200

    feat: remove animations

--- generate_images.py
+++ generate_images.py
@@ -63,7 +63,6 @@ async def generate_languages(s: Stats) -> None:
     sorted_languages = sorted(
         (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
     )
-    delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
         color = color if color is not None else "#000000"
@@ -73,7 +72,7 @@ async def generate_languages(s: Stats) -> None:
             f'class="progress-item"></span>'
         )
         lang_list += f"""
-<li style="animation-delay: {i * delay_between}ms;">
+<li>
 <svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{color};"
 viewBox="0 0 16 16" version="1.1" width="16" height="16"><path
 fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
